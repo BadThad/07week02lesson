7101b92050760646a4496d84e74afb914a929d20
Peter Fänge <pellefange@gmail.com>
2025-03-08 22:07:10 +0100

    first commit, made some of the basic functions

---
+++ randomadvice.js
@@ -0,0 +1,34 @@
+const flipCoin = new Promise((resolve, reject) => {
+    let result = Math.random();
+    if (result < 0.5) {
+      resolve("Heads");
+    } else {
+      reject("Tails");
+    }
+  });
+
+flipCoin
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const fetchAdviceById = (id) => {
+  fetch(`https://api.adviceslip.com/advice/${id}`).then((response) => {
+    if (!response.ok) {
+        throw new Error("No network response.");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    const advice = data.slip.advice;
+    console.log(`${advice}`)
+  })
+  .catch((error) => {
+    console.error("Sorry, we have no new advice for you.", error)
+  })
+};
+
+fetchAdviceById(220);
